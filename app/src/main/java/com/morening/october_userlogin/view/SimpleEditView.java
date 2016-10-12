e9cd992e38876af139b888d8dfb30ae292bf080c
morening <sunning_tj@163.com>
2016-10-12 23:07:46 +0800

    add show pwd icon & add attrs for SimpleEditView

--- app/src/main/java/com/morening/october_userlogin/view/SimpleEditView.java
+++ app/src/main/java/com/morening/october_userlogin/view/SimpleEditView.java
@@ -2,6 +2,7 @@ package com.morening.october_userlogin.view;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Color;
 import android.text.InputType;
 import android.text.TextUtils;
 import android.util.AttributeSet;
@@ -20,17 +21,22 @@ import com.morening.october_userlogin.R;
 
 public class SimpleEditView extends RelativeLayout {
 
-    private final int DEFAULT_TITLE_ICON = 0;
-    private final String DEFAULT_TITLE_TEXT = "Simple";
-    private final String DEFAULT_EDIT_HINT = "Why don't you have a try?";
-    private final boolean DEFAULT_EDIT_IS_PASSWORD = false;
+    private final int DEFAULT_STYLE_TITLE_ICON = 0;
+    private final String DEFAULT_STYLE_TITLE_TEXT = "Simple";
+    private final String DEFAULT_STYLE_EDIT_HINT = "Why don't you have a try?";
+    private final boolean DEFAULT_STYLE_EDIT_IS_PASSWORD = false;
+    private final boolean DEFAULT_STYLE_EDIT_SHOW_PASSWORD = false;
 
-    private int mTitleIcon = DEFAULT_TITLE_ICON;
-    private String mTitleText = DEFAULT_TITLE_TEXT;
-    private String mEditHint = DEFAULT_EDIT_HINT;
-    private boolean bIsPassword = DEFAULT_EDIT_IS_PASSWORD;
+    private int mTitleIconStyle = DEFAULT_STYLE_TITLE_ICON;
+    private String mTitleTextStyle = DEFAULT_STYLE_TITLE_TEXT;
+    private String mEditHintStyle = DEFAULT_STYLE_EDIT_HINT;
+    private boolean bIsPasswordStyle = DEFAULT_STYLE_EDIT_IS_PASSWORD;
+    private boolean bShowPwdStyle = DEFAULT_STYLE_EDIT_SHOW_PASSWORD;
 
     private View mView = null;
+    private EditText mEidtText = null;
+    private ImageView mShowPwdIcon = null;
+    private boolean bIsShowPwdEnabled = false;
 
     public SimpleEditView(Context context) {
         this(context, null);
@@ -53,8 +59,8 @@ public class SimpleEditView extends RelativeLayout {
     private void initViews() {
         ImageView titleIconImage = (ImageView) mView.findViewById(R.id.id_simple_editview_title_icon);
         TextView titleTextView = (TextView) mView.findViewById(R.id.id_simple_edittext_title_text);
-        if (mTitleIcon > 0){
-            titleIconImage.setBackground(getResources().getDrawable(mTitleIcon));
+        if (mTitleIconStyle > 0){
+            titleIconImage.setBackground(getResources().getDrawable(mTitleIconStyle));
             titleIconImage.setVisibility(VISIBLE);
 
             titleTextView.setVisibility(GONE);
@@ -62,32 +68,61 @@ public class SimpleEditView extends RelativeLayout {
             titleIconImage.setVisibility(GONE);
 
             titleTextView.setVisibility(VISIBLE);
-            titleTextView.setText(mTitleText);
+            titleTextView.setText(mTitleTextStyle);
         }
 
-        EditText editText = (EditText) mView.findViewById(R.id.id_simple_edittext_edit);
-        editText.setHint(mEditHint);
-        if (bIsPassword){
-            editText.setInputType(InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+        mEidtText = (EditText) mView.findViewById(R.id.id_simple_edittext_edit);
+        mEidtText.setHint(mEditHintStyle);
+        mShowPwdIcon = (ImageView) mView.findViewById(R.id.id_simple_edittext_show_pwd_icon);
+        if (bIsPasswordStyle){
+            mEidtText.setInputType(InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+            if (bShowPwdStyle){
+                mShowPwdIcon.setVisibility(VISIBLE);
+                mShowPwdIcon.setOnClickListener(new OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (bIsShowPwdEnabled){
+                            bIsShowPwdEnabled = false;
+                            mEidtText.setInputType(InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+                            mShowPwdIcon.getBackground().setTint(Color.BLUE);
+                        } else {
+                            bIsShowPwdEnabled = true;
+                            mEidtText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                            mShowPwdIcon.getBackground().setTint(Color.GREEN);
+                        }
+                    }
+                });
+            } else {
+                mShowPwdIcon.setVisibility(GONE);
+            }
         } else {
-            editText.setInputType(InputType.TYPE_CLASS_TEXT);
+            mEidtText.setInputType(InputType.TYPE_CLASS_TEXT);
+            mShowPwdIcon.setVisibility(GONE);
         }
     }
 
     private void obtainStyledAttrs(AttributeSet attrs) {
         TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.SimpleEditView);
 
-        mTitleIcon = ta.getResourceId(R.styleable.SimpleEditView_simple_edit_title_icon, mTitleIcon);
-        mTitleText = ta.getString(R.styleable.SimpleEditView_simple_edit_title_text);
-        if (TextUtils.isEmpty(mTitleText)){
-            mTitleText = DEFAULT_TITLE_TEXT;
+        mTitleIconStyle = ta.getResourceId(R.styleable.SimpleEditView_simple_edit_title_icon, mTitleIconStyle);
+        mTitleTextStyle = ta.getString(R.styleable.SimpleEditView_simple_edit_title_text);
+        if (TextUtils.isEmpty(mTitleTextStyle)){
+            mTitleTextStyle = DEFAULT_STYLE_TITLE_TEXT;
         }
-        mEditHint = ta.getString(R.styleable.SimpleEditView_simple_edit_edit_hint);
-        if (TextUtils.isEmpty(mEditHint)){
-            mEditHint = DEFAULT_EDIT_HINT;
+        mEditHintStyle = ta.getString(R.styleable.SimpleEditView_simple_edit_edit_hint);
+        if (TextUtils.isEmpty(mEditHintStyle)){
+            mEditHintStyle = DEFAULT_STYLE_EDIT_HINT;
         }
-        bIsPassword = ta.getBoolean(R.styleable.SimpleEditView_simple_edit_is_password, bIsPassword);
+        bIsPasswordStyle = ta.getBoolean(R.styleable.SimpleEditView_simple_edit_is_password, bIsPasswordStyle);
+        bShowPwdStyle = ta.getBoolean(R.styleable.SimpleEditView_simple_edit_show_pwd, bShowPwdStyle);
 
         ta.recycle();
+    }
+
+    public String getText(){
+        if (mEidtText == null){
+            return null;
+        }
+        return mEidtText.getText().toString();
     }
 }
