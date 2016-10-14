64355fcf0bb5cb72b8f8bc75d3767413ee2d30ae
morening <sunning_tj@163.com>
2016-10-14 23:22:29 +0800

    add SimpleEditView attrs and implement APIs, update build.gradle

--- app/src/main/java/com/morening/october_userlogin/MainActivity.java
+++ app/src/main/java/com/morening/october_userlogin/MainActivity.java
@@ -3,16 +3,24 @@ package com.morening.october_userlogin;
 import android.app.Activity;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.morening.october_userlogin.view.AnimationImageButton;
+import com.morening.october_userlogin.view.SimpleEditView;
 
 public class MainActivity extends Activity {
+
+    private AnimationImageButton mImageButton = null;
+    private SimpleEditView mSimpleEditName = null;
+    private SimpleEditView mSimpleEditPassword = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,24 +32,52 @@ public class MainActivity extends Activity {
         getWindow().setStatusBarColor(Color.TRANSPARENT);
         getActionBar().hide();
 
-        final AnimationImageButton imageButton =
-                (AnimationImageButton) findViewById(R.id.id_user_login_animation_button_login);
-        imageButton.setOnClickListener(new View.OnClickListener() {
+        mImageButton = (AnimationImageButton) findViewById(R.id.id_user_login_animation_button_login);
+        mImageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                imageButton.startAnimation(new AnimationImageButton.onProgressStateCallback(){
+                doLogin();
+            }
+        });
 
-                    @Override
-                    public void onProgressStart() {
-                        //Todo implement operation when start progress
-                    }
+        mSimpleEditName = (SimpleEditView) findViewById(R.id.id_user_login_simple_edit_name);
+        mSimpleEditPassword = (SimpleEditView) findViewById(R.id.id_user_login_simple_edit_password);
+        mSimpleEditName.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_NEXT){
+                    mSimpleEditPassword.requestFocus();
+                    mSimpleEditPassword.setSelection(mSimpleEditPassword.getCount());
+                }
 
-                    @Override
-                    public void onProgressEnd() {
-                        //Todo implement operation when end progress
-                    }
-                });
+                return false;
+            }
+        });
+        mSimpleEditPassword.setOnEditorActionListener(new EditText.OnEditorActionListener(){
 
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE){
+                    doLogin();
+                }
+                return false;
+            }
+        });
+    }
+
+    private void doLogin() {
+        // Todo do login logic
+
+        mImageButton.startAnimation(new AnimationImageButton.onProgressStateCallback(){
+
+            @Override
+            public void onProgressStart() {
+                //Todo implement operation when start progress
+            }
+
+            @Override
+            public void onProgressEnd() {
+                //Todo implement operation when end progress
             }
         });
     }
--- app/src/main/java/com/morening/october_userlogin/view/SimpleEditView.java
+++ app/src/main/java/com/morening/october_userlogin/view/SimpleEditView.java
@@ -5,15 +5,21 @@ import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.text.InputType;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
 import com.morening.october_userlogin.R;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by morening on 2016/10/8.
@@ -26,17 +32,21 @@ public class SimpleEditView extends RelativeLayout {
     private final String DEFAULT_STYLE_EDIT_HINT = "Why don't you have a try?";
     private final boolean DEFAULT_STYLE_EDIT_IS_PASSWORD = false;
     private final boolean DEFAULT_STYLE_EDIT_SHOW_PASSWORD = false;
+    private final int DEFAULT_STYLE_IME_OPTION = EditorInfo.IME_ACTION_NONE;
 
     private int mTitleIconStyle = DEFAULT_STYLE_TITLE_ICON;
     private String mTitleTextStyle = DEFAULT_STYLE_TITLE_TEXT;
     private String mEditHintStyle = DEFAULT_STYLE_EDIT_HINT;
     private boolean bIsPasswordStyle = DEFAULT_STYLE_EDIT_IS_PASSWORD;
     private boolean bShowPwdStyle = DEFAULT_STYLE_EDIT_SHOW_PASSWORD;
+    private int mImeOption = DEFAULT_STYLE_IME_OPTION;
 
     private View mView = null;
-    private EditText mEidtText = null;
+    private EditText mEditText = null;
     private ImageView mShowPwdIcon = null;
     private boolean bIsShowPwdEnabled = false;
+
+    private Map<String, Integer> mImeOptionMap = null;
 
     public SimpleEditView(Context context) {
         this(context, null);
@@ -71,11 +81,12 @@ public class SimpleEditView extends RelativeLayout {
             titleTextView.setText(mTitleTextStyle);
         }
 
-        mEidtText = (EditText) mView.findViewById(R.id.id_simple_edittext_edit);
-        mEidtText.setHint(mEditHintStyle);
+        mEditText = (EditText) mView.findViewById(R.id.id_simple_edittext_edit);
+        mEditText.setHint(mEditHintStyle);
+        mEditText.setImeOptions(mImeOption);
         mShowPwdIcon = (ImageView) mView.findViewById(R.id.id_simple_edittext_show_pwd_icon);
         if (bIsPasswordStyle){
-            mEidtText.setInputType(InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+            mEditText.setInputType(InputType.TYPE_NUMBER_VARIATION_PASSWORD);
             if (bShowPwdStyle){
                 mShowPwdIcon.setVisibility(VISIBLE);
                 mShowPwdIcon.setOnClickListener(new OnClickListener() {
@@ -83,11 +94,11 @@ public class SimpleEditView extends RelativeLayout {
                     public void onClick(View v) {
                         if (bIsShowPwdEnabled){
                             bIsShowPwdEnabled = false;
-                            mEidtText.setInputType(InputType.TYPE_NUMBER_VARIATION_PASSWORD);
+                            mEditText.setInputType(InputType.TYPE_NUMBER_VARIATION_PASSWORD);
                             mShowPwdIcon.getBackground().setTint(Color.BLUE);
                         } else {
                             bIsShowPwdEnabled = true;
-                            mEidtText.setInputType(InputType.TYPE_CLASS_NUMBER);
+                            mEditText.setInputType(InputType.TYPE_CLASS_NUMBER);
                             mShowPwdIcon.getBackground().setTint(Color.GREEN);
                         }
                     }
@@ -96,7 +107,7 @@ public class SimpleEditView extends RelativeLayout {
                 mShowPwdIcon.setVisibility(GONE);
             }
         } else {
-            mEidtText.setInputType(InputType.TYPE_CLASS_TEXT);
+            mEditText.setInputType(InputType.TYPE_CLASS_TEXT);
             mShowPwdIcon.setVisibility(GONE);
         }
     }
@@ -104,25 +115,89 @@ public class SimpleEditView extends RelativeLayout {
     private void obtainStyledAttrs(AttributeSet attrs) {
         TypedArray ta = getContext().obtainStyledAttributes(attrs, R.styleable.SimpleEditView);
 
-        mTitleIconStyle = ta.getResourceId(R.styleable.SimpleEditView_simple_edit_title_icon, mTitleIconStyle);
-        mTitleTextStyle = ta.getString(R.styleable.SimpleEditView_simple_edit_title_text);
+        mTitleIconStyle = ta.getResourceId(R.styleable.SimpleEditView_title_icon, mTitleIconStyle);
+        mTitleTextStyle = ta.getString(R.styleable.SimpleEditView_title_text);
         if (TextUtils.isEmpty(mTitleTextStyle)){
             mTitleTextStyle = DEFAULT_STYLE_TITLE_TEXT;
         }
-        mEditHintStyle = ta.getString(R.styleable.SimpleEditView_simple_edit_edit_hint);
+        mEditHintStyle = ta.getString(R.styleable.SimpleEditView_edit_hint);
         if (TextUtils.isEmpty(mEditHintStyle)){
             mEditHintStyle = DEFAULT_STYLE_EDIT_HINT;
         }
-        bIsPasswordStyle = ta.getBoolean(R.styleable.SimpleEditView_simple_edit_is_password, bIsPasswordStyle);
-        bShowPwdStyle = ta.getBoolean(R.styleable.SimpleEditView_simple_edit_show_pwd, bShowPwdStyle);
+        bIsPasswordStyle = ta.getBoolean(R.styleable.SimpleEditView_enable_password, bIsPasswordStyle);
+        bShowPwdStyle = ta.getBoolean(R.styleable.SimpleEditView_show_password, bShowPwdStyle);
+
+        initImeOptionMap();
+        String imeOption = ta.getString(R.styleable.SimpleEditView_ime_option);
+        mImeOption = getImeOption(imeOption);
 
         ta.recycle();
     }
 
+    private void initImeOptionMap() {
+        mImeOptionMap = new HashMap<>();
+        mImeOptionMap.put("actionUnspecified", EditorInfo.IME_ACTION_UNSPECIFIED);
+        mImeOptionMap.put("actionDone", EditorInfo.IME_ACTION_DONE);
+        mImeOptionMap.put("actionGo", EditorInfo.IME_ACTION_GO);
+        mImeOptionMap.put("actionNext", EditorInfo.IME_ACTION_NEXT);
+        mImeOptionMap.put("actionNone", EditorInfo.IME_ACTION_NONE);
+        mImeOptionMap.put("actionPrevious", EditorInfo.IME_ACTION_PREVIOUS);
+        mImeOptionMap.put("actionSearch", EditorInfo.IME_ACTION_SEARCH);
+        mImeOptionMap.put("actionSend", EditorInfo.IME_ACTION_SEND);
+    }
+
+    private int getImeOption(String imeOption) {
+
+        return mImeOptionMap.get(imeOption);
+    }
+
     public String getText(){
-        if (mEidtText == null){
+        if (mEditText == null){
             return null;
         }
-        return mEidtText.getText().toString();
+        mEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                return false;
+            }
+        });
+        return mEditText.getText().toString();
+    }
+
+    public void setOnEditorActionListener(final EditText.OnEditorActionListener listener){
+        if (mEditText != null){
+            mEditText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    return listener.onEditorAction(v, actionId, event);
+                }
+            });
+        }
+    }
+
+    public void setImeOptions(int imeOptions){
+        if (mEditText != null){
+            mEditText.setImeOptions(imeOptions);
+            mImeOption = imeOptions;
+        }
+    }
+
+    public void addTextChangedListener(TextWatcher watcher){
+        if (mEditText != null){
+            mEditText.addTextChangedListener(watcher);
+        }
+    }
+
+    public void setSelection(int index){
+        if (mEditText != null){
+            mEditText.setSelection(index);
+        }
+    }
+
+    public int getCount(){
+        if (mEditText == null || mEditText.getText() == null){
+            return 0;
+        }
+        return mEditText.getText().length();
     }
 }
