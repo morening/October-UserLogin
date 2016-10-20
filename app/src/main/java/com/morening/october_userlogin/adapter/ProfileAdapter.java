026ff7db47340250bd5f83b3caa8b2e305c90a10
morening <sunning_tj@163.com>
2016-10-20 22:45:55 +0800

    add profile recyclerview adapter

---
+++ app/src/main/java/com/morening/october_userlogin/adapter/ProfileAdapter.java
@@ -0,0 +1,62 @@
+package com.morening.october_userlogin.adapter;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.util.Pair;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.morening.october_userlogin.R;
+
+import java.util.List;
+
+/**
+ * Created by morening on 2016/10/20.
+ */
+public class ProfileAdapter extends RecyclerView.Adapter<ProfileAdapter.ProfileViewHolder> {
+
+    private Context mContext = null;
+    private List<Pair<String, String>> mDatas = null;
+
+    public ProfileAdapter(Context context, List<Pair<String, String>> info) {
+        mContext = context;
+        mDatas = info;
+    }
+
+    @Override
+    public ProfileViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View view = LayoutInflater.from(mContext).inflate(R.layout.profile_item_layout, parent, false);
+
+        return new ProfileViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ProfileViewHolder holder, int position) {
+        holder.title.setText(mDatas.get(position).first);
+        holder.content.setText(mDatas.get(position).second);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
+
+    class ProfileViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView title = null;
+        public TextView content = null;
+        public ImageView editIcon = null;
+
+        public ProfileViewHolder(View itemView) {
+            super(itemView);
+            title = (TextView) itemView.findViewById(R.id.id_profile_item_title);
+            content = (TextView) itemView.findViewById(R.id.id_profile_item_content);
+            editIcon = (ImageView) itemView.findViewById(R.id.id_profile_item_edit);
+        }
+    }
+
+}
--- app/src/main/java/com/morening/october_userlogin/fragment/ProfileFragment.java
+++ app/src/main/java/com/morening/october_userlogin/fragment/ProfileFragment.java
@@ -1,13 +1,25 @@
 package com.morening.october_userlogin.fragment;
 
 
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.Toolbar;
 
 import com.morening.october_userlogin.R;
+import com.morening.october_userlogin.adapter.DividerItemDecoration;
+import com.morening.october_userlogin.adapter.ProfileAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -15,6 +27,8 @@ import com.morening.october_userlogin.R;
 public class ProfileFragment extends Fragment {
 
     private View mView = null;
+    private Toolbar mToolbar = null;
+    private RecyclerView mRecyclerView = null;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -27,7 +41,30 @@ public class ProfileFragment extends Fragment {
 
         mView = inflater.inflate(R.layout.fragment_profile, container, false);
 
+        setupToolbar();
+        setupRecyclerView();
+
         return mView;
+    }
+
+    private void setupRecyclerView() {
+        mRecyclerView = (RecyclerView) mView.findViewById(R.id.id_profile_recyclerview);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        List<Pair<String, String>> info = new ArrayList<>();
+        loadUserInfo(info);
+        mRecyclerView.setAdapter(new ProfileAdapter(getContext(), info));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL_LIST));
+    }
+
+    private void loadUserInfo(List<Pair<String, String>> info) {
+        info.add(Pair.create("Name", "morening"));
+        info.add(Pair.create("ID", "morening"));
+        info.add(Pair.create("Barcode", "morening"));
+        info.add(Pair.create("Address", "China"));
+    }
+
+    private void setupToolbar() {
+        mToolbar = (Toolbar) mView.findViewById(R.id.id_profile_toolbar);
     }
 
 }
